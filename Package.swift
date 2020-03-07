66b91dac6c31e03cd251f585d8cafd1c4ead6dbd
Josh Wright <joshuawright11@gmail.com>
2020-03-06 17:39:59 -0800

    Fix

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc"),
         .package(url: "https://github.com/vapor/queues.git", from: "1.0.0-rc"),
-        .package(url: "https://github.com/joshuawright11/redis-kit.git", .branch("master")),
+        .package(url: "https://github.com/joshuawright11/redis-kit.git", .branch("reset")),
     ],
     targets: [
         .target(
